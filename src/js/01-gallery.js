fb12da20aaa78849863692ff1b8802287d3b19d1
Anna Kravets <annakravets1906@gmail.com>
2023-03-11 13:38:18 +0200

    Додано завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galerryImages = document.querySelector('.gallery');
+const galleryMarkUp = createGallery(galleryItems);
+galerryImages.innerHTML = galleryMarkUp;
+
+function createGallery(galleryItems) {
+  const imagesMarkUp = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+  return imagesMarkUp;
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionDelay: 250,
+});
+
+gallery.elements.forEach(element => {
+  element.firstElementChild.title = element.firstElementChild.alt;
+});
