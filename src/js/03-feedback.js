ba26dfceaec231456c6aafa4af8a8293d571a051
Anna Kravets <annakravets1906@gmail.com>
2023-03-12 08:59:51 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,49 @@
-import throttle from "lodash.throttle";
+// import throttle from "lodash.throttle";
 
-const refs = {
-  form: document.querySelector(".feedback-form"),
-  // input: document.querySelector('.feedback-form input'),
-  // textarea: document.querySelector('.feedback-form textarea'),
-};
+// const refs = {
+//   form: document.querySelector(".feedback-form"),
+// };
 
-refs.form.addEventListener("input", throttle(onInputForm, 500));
-refs.form.addEventListener("submit", onFormSubmit);
+// refs.form.addEventListener("input", throttle(onInputForm, 500));
+// refs.form.addEventListener("submit", onFormSubmit);
 
-const savedItems = {};
-onOutput();
+// const savedItems = {};
+// onOutput();
 
-function onInputForm(event) {
-  const {
-    elements: { email, message },
-  } = event.target;
+// function onInputForm(event) {
+//   const {
+//     elements: { email, message },
+//   } = event.target;
 
-  console.log(event.target);
+//   console.log(event.target);
 
-  localStorage.setItem(
-    "feedback-form-state",
-    JSON.stringify({ email: email.value, message: message.value })
-  );
-}
+//   localStorage.setItem(
+//     "feedback-form-state",
+//     JSON.stringify({ email: email.value, message: message.value })
+//   );
+// }
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.target.reset();
-  localStorage.removeItem("feedback-form-state");
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   event.target.reset();
+//   localStorage.removeItem("feedback-form-state");
 
-  console.log(savedItems);
+//   console.log(savedItems);
 
-  // console.log('email' email.value);
-  // console.log('message' message.value);
-}
+//   // console.log('email' email.value);
+//   // console.log('message' message.value);
+// }
 
-function onOutput() {
-  savedItems = localStorage.getItem("feedback-form-state");
+// function onOutput() {
+//   savedItems = localStorage.getItem("feedback-form-state");
 
-  if (savedItems) {
-    console.log(savedItems);
-    const savesItems = JSON.parse(savedItems);
-    console.log("email:", savesItems.email);
+//   if (savedItems) {
+//     console.log(savedItems);
+//     const savesItems = JSON.parse(savedItems);
+//     console.log("email:", savesItems.email);
 
-    // console.log(savesItems.email);
+//     // console.log(savesItems.email);
+//   }
+// }
 
-    // savesItems.email.value = savedItems;
-  }
-}
+console.log(1253);
