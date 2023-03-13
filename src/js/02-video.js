4eaee65f5d98a0cd173a3414d5d1ebb62071e894
Anna Kravets <annakravets1906@gmail.com>
2023-03-13 14:02:14 +0200

    Виправлено зауваження ментора

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on(
   'timeupdate',
   throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+    localStorage.setItem(CURRENT_TIME, data.seconds);
   }, 1000)
 );
 
-const savedTimes = localStorage.getItem('videoplayer-current-time');
+const savedTimes = localStorage.getItem(CURRENT_TIME || 0);
 player.setCurrentTime(savedTimes);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,40 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 
-form.addEventListener("input", throttle(onInputForm, 500));
-form.addEventListener("submit", onFormSubmit);
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onFormSubmit);
 
-const formData = {};
-let savesItems = {};
+let formData = {};
+onOutput();
 
 function onInputForm(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-const savedItems = localStorage.getItem("feedback-form-state");
+function onOutput(params) {}
+const savedItems = localStorage.getItem('feedback-form-state');
 if (savedItems) {
-  savesItems = JSON.parse(savedItems);
-  form.email.value = savesItems.email;
-  form.message.value = savesItems.message;
+  try {
+    const formData = JSON.parse(savedItems);
+
+    const entries = Object.entries(formData);
+    entries.forEach(({ name, value }) => {
+      formData[name] = value;
+      form.email.value = formData.email;
+      form.message.value = formData.message;
+    });
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.target.reset();
-  localStorage.removeItem("feedback-form-state");
+  localStorage.removeItem('feedback-form-state');
 
-  console.log({ email: savesItems.email, message: savesItems.message });
+  console.log({ email: formData.email, message: formData.message });
 }
